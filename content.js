66c40496092009df707045c97cf49c4e2e2a09b6
Veronica <veronica@setsutamas-MacBook-puro.local>
2019-04-05 20:49:01 +0900

    Set default interval to 15 sec

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function()
 {
-  chrome.storage.sync.get({input: 'Default', interval: 5000}, function(items){
+  chrome.storage.sync.get({input: 'Default', interval: 15000}, function(items){
     setTimeout(function() {
       var input = document.getElementsByTagName("input")[0];
       input.value = items.input;
